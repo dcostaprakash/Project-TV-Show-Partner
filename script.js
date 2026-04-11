48a6094952133b449ad144dc441bc2ec724a6e11
dcostaprakash <starfinhub@gmail.com>
2026-04-11 12:00:47 +0100

    Added a search filter for episodes and made the previous code into cards for level 200

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+let allEpisodes = [];
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  createSearchBox();
 }
 
 function makePageForEpisodes(episodeList) {
@@ -9,13 +11,13 @@ function makePageForEpisodes(episodeList) {
   // Clear existing content
   rootElem.innerHTML = "";
 
-  episodeList.forEach(function(episode) {
-
+  episodeList.forEach(function (episode) {
     const season = String(episode.season).padStart(2, "0");
     const number = String(episode.number).padStart(2, "0");
     const episodeCode = `S${season}E${number}`;
 
     const episodeDiv = document.createElement("div");
+    episodeDiv.className = "card";
 
     episodeDiv.innerHTML = `
   <h2>${episode.name}</h2>
@@ -27,6 +29,37 @@ function makePageForEpisodes(episodeList) {
 `;
 
     rootElem.appendChild(episodeDiv);
+  });
+}
+function createSearchBox() {
+  const rootElem = document.getElementById("root");
+
+  const container = document.createElement("div");
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "Search episodes...";
+
+  const counter = document.createElement("p");
+
+  container.appendChild(input);
+  container.appendChild(counter);
+
+  document.body.insertBefore(container, rootElem);
+
+  input.addEventListener("input", function () {
+    const searchTerm = input.value.toLowerCase();
+
+    const filtered = allEpisodes.filter(function (episode) {
+      const name = episode.name.toLowerCase();
+      const summary = episode.summary.toLowerCase();
+
+      return name.includes(searchTerm) || summary.includes(searchTerm);
+    });
+
+    makePageForEpisodes(filtered);
+
+    counter.textContent = `${filtered.length} / ${allEpisodes.length} episodes`;
   });
 }
 
